38e5c2d50f8a32906d308e195485d51ad01f26e5
Christopher <chris.tryber@gmail.com>
2022-01-27 15:43:32 -0300

    Separei as pastas e criei o server

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
