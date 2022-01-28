ecb77f26d46ba3b8b411b773cda7ed2c2e5113f2
Christopher <chris.tryber@gmail.com>
2022-01-28 12:10:16 -0300

    Separei as pastas front e back

---
+++ front-end/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        users: ['Chris'],
+        messages: [{
+            msg: 'Hey there',
+            time: '11:53 PM'
+        }]
+    }
+})
